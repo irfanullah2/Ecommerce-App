c2807dbf7158cfa5d9669cb0f16e6bd5d3269c25
IRFAN ULLAH <129148590+irfanullah2@users.noreply.github.com>
2024-10-23 15:27:06 +0500

    Update ForgotPassword.js

--- client/src/pages/Auth/ForgotPassword.js
+++ client/src/pages/Auth/ForgotPassword.js
@@ -18,7 +18,7 @@ const ForgotPassword = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('/api/v1/auth/forgot-password',
+      const res = await axios.post('https://ecommerce-app-server-topaz.vercel.app/forgot-password',
          { 
             email,
             newPassword,
