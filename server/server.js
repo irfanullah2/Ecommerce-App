aa28aca35884ace86375b07bb29478696439fc01
IRFAN ULLAH <129148590+irfanullah2@users.noreply.github.com>
2024-10-23 15:11:04 +0500

    client path is updated

--- server/server.js
+++ server/server.js
@@ -23,6 +23,7 @@ const app = express();
 
 // middlewares
 app.use(cors());
+
 app.use(express.json())
 app.use(morgan('dev'));
 
@@ -36,10 +37,10 @@ app.use('/api/v1/product', productRoutes);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('*' , function(req, res){
-    res.sendFile(path.join(__dirname , './client/build/index.html'));
+    res.sendFile(path.join(__dirname , '../client/build/index.html'));
 })
 
 
